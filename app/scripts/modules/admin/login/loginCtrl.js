d3e0899fea30d8affa7ec199521459406de96304
ankit <ankit.garg@upshotlive.com>
2015-08-21 19:08:41 +0530

    Integrated login page
    Integrated login page with basic validations to the application.

--- app/scripts/modules/admin/login/loginCtrl.js
+++ app/scripts/modules/admin/login/loginCtrl.js
@@ -4,8 +4,16 @@ define(['app'], function (app) {
   app.controller('LoginCtrl',['$scope','UTILS',
 	function ($scope,UTILS) {
 
+		$scope.loginRequest = {};
 		// UTILS.setInSession('test','value');
 		// console.log(UTILS.getFromSession('test'));
 		// console.log(UTILS.encryptCredential('12345'));
+
+		$scope.login = function(value){
+			if(value){
+				alert("Login Successful");
+			}
+		};
+
 	}]);
 });
